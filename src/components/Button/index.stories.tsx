332c147e24b74cba86e5035ccaebfffc98bb90c2
yoonncho <nicealice99@gmail.com>
2025-01-28 14:31:49 +0900

    feat: 예시 Button 컴포넌트 스토리북 추가(#1)

---
+++ src/components/Button/index.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import Button from '.'
+
+const meta = {
+  title: 'components/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    variant: { control: 'radio' },
+    size: { control: 'radio' },
+  },
+  args: {
+    onClick: fn(),
+    children: 'Button',
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const BasicButton: Story = {
+  args: {
+    variant: 'filled',
+    size: 'medium',
+  },
+}
