9a8fce079e53c98fdc9611b3466bb405e5132773
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-30 13:50:06 +0900

    chore: svgr 라이브러리 설치 및 초기설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HomeIcon from '@/assets/icons/home.svg'
+
 export default function Home() {
   return (
     <div className='bg-white'>
+      <HomeIcon />
       <h2>Leev</h2>
     </div>
   )
