9317df5fc6b67b9e0e9b214f5d524f9802377d68
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-31 17:38:32 +0900

    chore: (with-sidebar) 디렉토리 생성 및 페이지 이동

--- src/app/(with-sidebar)/page.tsx
+++ src/app/(with-sidebar)/page.tsx
@@ -1,9 +1,6 @@
-import HomeIcon from '@/assets/icons/home.svg'
-
 export default function Home() {
   return (
     <div className='bg-white'>
-      <HomeIcon />
       <h2>Leev</h2>
     </div>
   )
