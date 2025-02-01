ec1fe76c479b070fdd2feba9ffe6479ddc4e6620
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-01 13:38:35 +0900

    refactor: svg아이콘 import명 -icon 추가 통일

--- src/app/(with-sidebar)/_components/NavigateList/index.tsx
+++ src/app/(with-sidebar)/_components/NavigateList/index.tsx
@@ -3,14 +3,14 @@ import SidebarItem from '../SidebarItem'
 import NotificationLink from '../SidebarItem/NotificationLink'
 
 import HomeIcon from '@/assets/icons/sidebar/non-active/home.svg'
-import Edit from '@/assets/icons/sidebar/non-active/edit.svg'
-import Profile from '@/assets/icons/sidebar/non-active/profile.svg'
-import ProfileUser from '@/assets/icons/sidebar/non-active/profile-user.svg'
+import EditIcon from '@/assets/icons/sidebar/non-active/edit.svg'
+import ProfileIcon from '@/assets/icons/sidebar/non-active/profile.svg'
+import ProfileUserIcon from '@/assets/icons/sidebar/non-active/profile-user.svg'
 
 import ActiveHomeIcon from '@/assets/icons/sidebar/active/home-fiiled.svg'
-import ActiveEdit from '@/assets/icons/sidebar/active/edit-filled.svg'
-import ActiveProfile from '@/assets/icons/sidebar/active/profile-filled.svg'
-import ActiveProfileUser from '@/assets/icons/sidebar/active/profile-user-filled.svg'
+import ActiveEditIcon from '@/assets/icons/sidebar/active/edit-filled.svg'
+import ActiveProfileIcon from '@/assets/icons/sidebar/active/profile-filled.svg'
+import ActiveProfileUserIcon from '@/assets/icons/sidebar/active/profile-user-filled.svg'
 
 const NavigateList = () => {
   // Todo: 각 페이지 path 설정 후 변경
@@ -25,21 +25,21 @@ const NavigateList = () => {
       <SidebarItem
         path={'/'}
         contentName={'회고 스페이스'}
-        activeIcon={<ActiveEdit />}
-        nonActiveIcon={<Edit />}
+        activeIcon={<ActiveEditIcon />}
+        nonActiveIcon={<EditIcon />}
       />
       <SidebarItem
         path={'/'}
         contentName={'모임'}
-        activeIcon={<ActiveProfileUser />}
-        nonActiveIcon={<ProfileUser />}
+        activeIcon={<ActiveProfileUserIcon />}
+        nonActiveIcon={<ProfileUserIcon />}
       />
       <NotificationLink />
       <SidebarItem
         path={'/'}
         contentName={'내 정보'}
-        activeIcon={<ActiveProfile />}
-        nonActiveIcon={<Profile />}
+        activeIcon={<ActiveProfileIcon />}
+        nonActiveIcon={<ProfileIcon />}
       />
     </nav>
   )
