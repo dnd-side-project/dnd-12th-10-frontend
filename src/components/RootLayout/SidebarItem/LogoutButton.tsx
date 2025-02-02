e9c2b4b856f372bc2e8cf29a27ff53aa90b7d5c0
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-03 00:59:14 +0900

    refactor: svg아이콘 import명 -Icon 추가 통일

--- src/components/RootLayout/SidebarItem/LogoutButton.tsx
+++ src/components/RootLayout/SidebarItem/LogoutButton.tsx
@@ -1,10 +1,10 @@
 import { cn } from '@/utils/cn'
-import Logout from '@/assets/icons/sidebar/logout.svg'
+import LogoutIcon from '@/assets/icons/sidebar/logout.svg'
 
 const LogoutButton = () => {
   return (
     <button className={cn('flex gap-2', 'mt-auto', 'text-title03')}>
-      <Logout />
+      <LogoutIcon />
       <span className='text-white'>로그아웃</span>
     </button>
   )
--- src/components/RootLayout/SidebarItem/NotificationLink.tsx
+++ src/components/RootLayout/SidebarItem/NotificationLink.tsx
@@ -2,16 +2,16 @@
 
 import { cn } from '@/utils/cn'
 import SidebarItem from './index'
-import Notification from '@/assets/icons/sidebar/non-active/notification.svg'
-import ActiveNotification from '@/assets/icons/sidebar/active/notification-filled.svg'
+import NotificationIcon from '@/assets/icons/sidebar/non-active/notification.svg'
+import ActiveNotificationIcon from '@/assets/icons/sidebar/active/notification-filled.svg'
 
 const NotificationLink = () => {
   return (
     <SidebarItem
       path='/3'
       contentName='알림'
-      activeIcon={<ActiveNotification />}
-      nonActiveIcon={<Notification />}
+      activeIcon={<ActiveNotificationIcon />}
+      nonActiveIcon={<NotificationIcon />}
     >
       {/*Todo:알림 갯수 api 연결*/}
       <span
