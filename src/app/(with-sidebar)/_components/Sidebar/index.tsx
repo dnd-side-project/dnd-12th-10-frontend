a8e9de00a576b0a5c28bd9e3c58d39af2d04345c
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-02 17:26:18 +0900

    refactor: search-button 컴포넌트 삭제

--- src/app/(with-sidebar)/_components/Sidebar/index.tsx
+++ src/app/(with-sidebar)/_components/Sidebar/index.tsx
@@ -1,7 +1,6 @@
 import { cn } from '@/utils/cn'
 import LogoIcon from '@/assets/icons/logo.svg'
 import LogoutButton from '@/app/(with-sidebar)/_components/SidebarItem/LogoutButton'
-import SearchButton from '@/app/(with-sidebar)/_components/SidebarItem/SearchButton'
 import NavigateList from '@/app/(with-sidebar)/_components/NavigateList'
 const Sidebar = () => {
   return (
@@ -13,8 +12,7 @@ const Sidebar = () => {
         'bg-gray-900',
       )}
     >
-      <LogoIcon />
-      <SearchButton />
+      <LogoIcon className='mb-[40px]' />
       <NavigateList />
       <LogoutButton />
     </aside>
