2cfc07429b09774b8ce69ef94ea3e7aa893b2638
yoonncho <nicealice99@gmail.com>
2025-02-10 00:33:05 +0900

    chore: 테스트를 위한 텍스트 추가 (#10)

--- src/app/_components/Banner.tsx
+++ src/app/_components/Banner.tsx
@@ -16,7 +16,7 @@ const Banner = () => {
       <div>
         <h3 className='text-title01 text-blue-600'>
           막막했던 회고가 특별해지는 순간 <br /> 비슷한 경험을 가진 동료들의
-          인사이트를 만나보세요.
+          인사이트를 만나보세요.11
         </h3>
         <p className='mt-2 text-blue-900 text-body01 mb-[26px]'>
           인기 회고 모임을 찾거나, 나만의 모임을 만들어볼 수 있어요!
