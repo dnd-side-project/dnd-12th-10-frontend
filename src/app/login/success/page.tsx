caa80979d38683d0b0a0194047b1164c053ea3a1
yoonncho <nicealice99@gmail.com>
2025-02-09 21:45:45 +0900

    fix: 로그인 성공 페이지에서 Suspense 감싸기

--- src/app/login/success/page.tsx
+++ src/app/login/success/page.tsx
@@ -1,37 +1,12 @@
-'use client'
-import { useEffect } from 'react'
-import { toast } from 'react-hot-toast'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { URL_PATH } from '@/consts/urls'
-import { useAuthStore } from '@/store/auth'
-import { setAccessToken } from '@/lib/axios'
+import { Suspense } from 'react'
+import LoginSuccess from './LoginSuccess'
 
 const SuccessPage = () => {
-  const { setIsLogin } = useAuthStore()
-  const router = useRouter()
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    try {
-      const accessToken = searchParams.get('access_token')
-      const isRegistered = searchParams.get('isRegistered')
-
-      if (accessToken && isRegistered) {
-        setAccessToken(accessToken)
-        setIsLogin(true)
-        router.replace(isRegistered ? URL_PATH.Home : URL_PATH.Signup)
-      } else {
-        toast.error('로그인에 실패했습니다')
-        router.replace(URL_PATH.Login)
-      }
-    } catch (err) {
-      console.error(err)
-      toast.error('로그인에 실패했습니다')
-      router.replace(URL_PATH.Login)
-    }
-  }, [searchParams, setAccessToken])
-
-  return <div>Success</div>
+  return (
+    <Suspense>
+      <LoginSuccess />
+    </Suspense>
+  )
 }
 
 export default SuccessPage
