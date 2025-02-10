5ba8a8e2de7b5d72243d0896a8a7a2058ac6dd56
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-10 17:04:42 +0900

    feat: 모임 상세페이지 memoList 마지막 글쓰기 버튼 추가 (#26)

--- src/app/groups/[id]/_components/MemoList.tsx
+++ src/app/groups/[id]/_components/MemoList.tsx
@@ -10,6 +10,8 @@ import Dropdown from '@/components/Dropdown'
 import LikeIcon from '@/assets/icons/like.svg'
 import MessageIcon from '@/assets/icons/message.svg'
 import UserThumb from '@/assets/images/user-thumb.png'
+import Button from '@/components/Button'
+import EditIcon from '@/assets/icons/edit.svg'
 
 interface MemoListItemProps {
   title: string
@@ -40,6 +42,18 @@ const MemoList = ({ memoList }: { memoList: MemoListItemProps[] }) => {
         {memoList.map((memo, index) => (
           <MemoListItem key={index} {...memo} />
         ))}
+        <li className='flex flex-col items-center'>
+          <p className='text-title02 text-gray-500'>
+            모든 회고를 확인했습니다!
+          </p>
+          <p className='text-body02 text-gray-500 font-normal mb-6'>
+            이제 당신의 이야기를 들려주세요. ✍️
+          </p>
+          <Button color='primary' variant='subtle' size='medium'>
+            <EditIcon className='stroke-blue-500 mr-2' width={20} height={20} />
+            글 쓰기
+          </Button>{' '}
+        </li>
       </ul>
     </>
   )
