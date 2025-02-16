022cb3d2e2b74dbaa80fe7225f26a7b18707314d
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-17 00:02:11 +0900

    feat: JOB_LIST key 값 label 동일하게 변경 (#46)

--- src/app/login/signup/_consts/jobList.ts
+++ src/app/login/signup/_consts/jobList.ts
@@ -1,8 +1,8 @@
 export const JOB_LIST = [
-  { key: 'jobless', label: '취준생' },
-  { key: 'student', label: '학생' },
-  { key: 'frontend', label: '프론트엔드 개발자' },
-  { key: 'backend', label: '백엔드 개발자' },
-  { key: 'designer', label: 'UX/UI 디자이너' },
-  { key: 'content', label: '콘텐츠 디자이너' },
+  { key: '취준생', label: '취준생' },
+  { key: '학생', label: '학생' },
+  { key: '프론트엔드 개발자', label: '프론트엔드 개발자' },
+  { key: '프론트엔드 개발자', label: '백엔드 개발자' },
+  { key: 'UX/UI 디자이너', label: 'UX/UI 디자이너' },
+  { key: '콘텐츠 디자이너', label: '콘텐츠 디자이너' },
 ]
