0bc726ea49a53310726984574ba36ea8e1834aa6
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-17 18:12:54 +0900

    fix: JOB_LIST key 오타 수정 (#46)

--- src/app/login/signup/_consts/jobList.ts
+++ src/app/login/signup/_consts/jobList.ts
@@ -2,7 +2,7 @@ export const JOB_LIST = [
   { key: '취준생', label: '취준생' },
   { key: '학생', label: '학생' },
   { key: '프론트엔드 개발자', label: '프론트엔드 개발자' },
-  { key: '프론트엔드 개발자', label: '백엔드 개발자' },
+  { key: '백엔드 개발자', label: '백엔드 개발자' },
   { key: 'UX/UI 디자이너', label: 'UX/UI 디자이너' },
   { key: '콘텐츠 디자이너', label: '콘텐츠 디자이너' },
 ]
