46294e28a73d5f031580058736b51777297de3ad
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-19 14:55:56 +0900

    fix: signup 불필요한 try-catch 및 상태 검사 제거 - useSignupMutation에서 관리 (#46)

--- src/app/login/signup/_lib/index.ts
+++ src/app/login/signup/_lib/index.ts
@@ -3,11 +3,5 @@ import { API_PATH } from '@/consts/urls'
 import { UserCreateForm as UserCreateFormType } from '@/app/login/signup/_types'
 
 export const signup = async (data: UserCreateFormType) => {
-  try {
-    const response = await axiosInstance.patch(API_PATH.Signup, data)
-    return response.status === 200
-  } catch (error) {
-    console.error('Error during token reissue:', error)
-    throw error
-  }
+  return await axiosInstance.patch(API_PATH.Signup, data)
 }
