cf08d701839c038cc96b86fe82eef2782470867c
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-20 02:43:00 +0900

    chore: NotificationLink 컴포넌트 주석 처리 (#49)

--- src/components/RootLayout/NavigateList/index.tsx
+++ src/components/RootLayout/NavigateList/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { URL_PATH } from '@/consts/urls'
 import SidebarItem from '../SidebarItem'
-import NotificationLink from '@/components/RootLayout/SidebarItem/NotificationLink'
+// import NotificationLink from '@/components/RootLayout/SidebarItem/NotificationLink'
 
 const NavigateList = () => {
   // Todo: 각 페이지 path 설정 후 변경
@@ -9,8 +9,12 @@ const NavigateList = () => {
     <nav>
       <SidebarItem path={URL_PATH.Home} contentName='홈' iconName='home' />
       <SidebarItem path='/1' contentName='회고 스페이스' iconName='edit' />
-      <SidebarItem path={URL_PATH.GroupList} contentName='모임' iconName='profile-user' />
-      <NotificationLink />
+      <SidebarItem
+        path={URL_PATH.GroupList}
+        contentName='모임'
+        iconName='profile-user'
+      />
+      {/*<NotificationLink />*/}
       <SidebarItem path='/4' contentName='내 정보' iconName='profile' />
     </nav>
   )
