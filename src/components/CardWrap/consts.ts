2b7a5ecfe6688f79cc0263475d78f86756a661ac
leeleeleeleejun <wnstjr6293@naver.com>
2025-02-19 21:18:20 +0900

    feat: CARD_WRAP_SIZE min-w 추가 (#49)

--- src/components/CardWrap/consts.ts
+++ src/components/CardWrap/consts.ts
@@ -1,6 +1,6 @@
 import { CardWrapProps } from './index'
 
 export const CARD_WRAP_SIZE: { [k in CardWrapProps['size']]: string } = {
-  medium: 'w-[242px]',
-  large: 'w-[328px]',
+  medium: 'min-w-[242px] w-[242px]',
+  large: 'min-w-[328px] w-[328px]',
 }
