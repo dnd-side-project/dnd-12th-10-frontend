8720a03b265cf370d5867d6c1f4be6c0ec649126
yoonncho <nicealice99@gmail.com>
2025-02-22 00:58:13 +0900

    feat: 모임 상세 api 연결 (#54)

---
+++ src/app/groups/[id]/_queries/useGetGroupInfo.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
+import { getGroupInfo } from '../_lib'
+
+/** 템플릿 개별 조회 */
+const useGetGroupInfo = (groupId: string) => {
+  return useQuery({
+    queryKey: ['getGroupInfo'],
+    queryFn: async () => await getGroupInfo(groupId),
+  })
+}
+
+export default useGetGroupInfo
--- src/app/groups/[id]/page.tsx
+++ src/app/groups/[id]/page.tsx
@@ -1,67 +1,16 @@
 import { Metadata } from 'next'
-import { cn } from '@/utils/cn'
-import { GROUP_DETAIL } from './_consts'
-import GroupHeading from './_components/GroupHeading'
-import GroupDescription from './_components/GroupDescription'
-import GroupInfoList from './_components/GroupInfoList'
-import NoMemoList from './_components/NoMemoList'
-import MemoList from './_components/MemoList'
+import GroupDetail from './GroupDetail'
 /**
  * 모임 상세 페이지
  */
 
 // Todo: api 연결 후 모임 이름으로 변경 필요
 export const metadata: Metadata = {
-  title: 'Leev | 모임이름',
+  title: 'Leev | 모임',
 }
 
 const GroupDetailPage = () => {
-  const {
-    groupName,
-    numOfMembers,
-    introduction,
-    tags,
-    isPublic,
-    description,
-    numOfMemos,
-    createdAtGroup,
-    latestUpdateTime,
-    memoList,
-    roll,
-  } = GROUP_DETAIL
-
-  return (
-    <div className={cn('px-[88px] py-[72px]')}>
-      <GroupHeading
-        groupName={groupName}
-        introduction={introduction}
-        tags={tags}
-        roll={roll}
-      />
-      <div
-        className={cn(
-          'flex flex-col gap-8',
-          'mt-12 p-6',
-          'bg-gray-50 ',
-          'rounded-lg',
-        )}
-      >
-        <GroupInfoList
-          numOfMembers={numOfMembers}
-          numOfMemos={numOfMemos}
-          createdAtGroup={createdAtGroup}
-          latestUpdateTime={latestUpdateTime}
-        />
-        {description && <GroupDescription description={description} />}
-      </div>
-
-      {memoList.length > 0 && (isPublic || roll) ? (
-        <MemoList memoList={memoList} />
-      ) : (
-        <NoMemoList isPublic={isPublic} roll={roll} />
-      )}
-    </div>
-  )
+  return <GroupDetail />
 }
 
 export default GroupDetailPage
