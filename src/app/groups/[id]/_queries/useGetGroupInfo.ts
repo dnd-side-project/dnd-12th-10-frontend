62823205552065acaf6cb743e4a8a8cbd321fdad
leeleeleeleejun <wnstjr6293@naver.com>
2025-03-27 20:40:09 +0900

    feat: UseGetGroupInfo 함수 groupId없을 경우 요청 하지 않도록 설정 - js doc 수정 (#71)

--- src/app/groups/[id]/_queries/useGetGroupInfo.ts
+++ src/app/groups/[id]/_queries/useGetGroupInfo.ts
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import { getGroupInfo } from '../_lib'
 
-/** 템플릿 개별 조회 */
+/** 그룹 개별 데이터 조회 */
 const useGetGroupInfo = (groupId: string) => {
   return useQuery({
     queryKey: ['getGroupInfo', { groupId }],
     queryFn: async () => await getGroupInfo(groupId),
+    enabled: !!groupId,
   })
 }
 
