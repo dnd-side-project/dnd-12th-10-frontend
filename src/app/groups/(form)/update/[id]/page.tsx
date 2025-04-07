4dcc250072ca9cd6939379b3b29f9180c4e2dd69
leeleeleeleejun <wnstjr6293@naver.com>
2025-04-07 14:48:03 +0900

    feat: 모임 정보 수정 페이지 제목(h1) 추가 (#71)

--- src/app/groups/(form)/update/[id]/page.tsx
+++ src/app/groups/(form)/update/[id]/page.tsx
@@ -1,7 +1,12 @@
 import GroupForm from '@/app/groups/(form)/GroupForm'
 
 const UpdateGroupPage = () => {
-  return <GroupForm />
+  return (
+    <div className='px-[88px] pt-[72px] pb-[66px]'>
+      <h1 className='text-display01 mb-6'>모임 정보 수정</h1>
+      <GroupForm />
+    </div>
+  )
 }
 
 export default UpdateGroupPage
