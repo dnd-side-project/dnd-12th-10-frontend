3a32e15af84acc7141118f1ebdd95269bd58a4dd
yoonncho <nicealice99@gmail.com>
2025-05-11 22:25:02 +0900

    feat: 댓글 수정 폼 추가 (#72)

---
+++ src/app/retrospects/[id]/_components/CommentUpdate.tsx
@@ -0,0 +1,73 @@
+import Button from '@/components/Button'
+import { cn } from '@/utils/cn'
+import { useForm } from 'react-hook-form'
+import { CommentUpdateForm } from '../types'
+import useUpdateCommentMutation from '../../_querys/useUpdateCommentMutation'
+import { useParams } from 'next/navigation'
+
+const CommentUpdate = ({
+  commentId,
+  initialValue,
+  onCloseInput,
+}: {
+  commentId: string
+  initialValue: string
+  onCloseInput: () => void
+}) => {
+  const retrospectId = useParams<{ id: string }>()?.id
+  const { mutate } = useUpdateCommentMutation(retrospectId)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid },
+  } = useForm<CommentUpdateForm>()
+
+  const onSubmit = (data: CommentUpdateForm) => {
+    mutate({ commentId, data }, { onSuccess: onCloseInput })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <textarea
+        defaultValue={initialValue}
+        className={cn(
+          'w-full max-h-[200px]',
+          'mt-6 py-4 px-6 ',
+          'bg-gray-50',
+          'rounded-md',
+          'text-body01 ',
+          'outline-none',
+        )}
+        {...register('content', { required: true })}
+      />
+      <div className='mt-4 flex justify-end gap-2'>
+        <Button
+          style={{
+            minWidth: '72px',
+          }}
+          color='primary'
+          variant='subtle'
+          size='small'
+          onClick={onCloseInput}
+        >
+          취소
+        </Button>
+        <Button
+          style={{
+            minWidth: '72px',
+          }}
+          color='primary'
+          variant='filled'
+          size='small'
+          type='submit'
+          disabled={!isDirty || !isValid}
+        >
+          확인
+        </Button>
+      </div>
+    </form>
+  )
+}
+
+export default CommentUpdate
