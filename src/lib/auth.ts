e727c598b495bc90c4031fbe738f960a426f4590
yoonncho <nicealice99@gmail.com>
2025-05-11 22:27:56 +0900

    fix: 토큰 갱신 무한 요청되는 이슈 수정 (#72)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,19 +2,36 @@ import { axiosInstance } from '@/lib/axios'
 import { API_PATH } from '@/consts/urls'
 import { setAccessToken } from '@/utils/auth'
 
+let tokenRefreshInProgress = false
+let tokenRefreshPromise: Promise<string> | null = null
+
 export const reissueToken = async (): Promise<string> => {
+  // 이미 토큰 재발급이 진행 중이면 진행 중인 Promise를 반환
+  if (tokenRefreshInProgress) {
+    return tokenRefreshPromise!
+  }
+
   try {
-    const response = await axiosInstance.get(API_PATH.ReissueToken)
+    tokenRefreshInProgress = true
+    tokenRefreshPromise = (async () => {
+      const response = await axiosInstance.get(API_PATH.ReissueToken)
 
-    let newAccessToken = response.headers['authorization']
-    if (newAccessToken?.startsWith('Bearer ')) {
-      newAccessToken = newAccessToken.substring(7)
-    }
+      let newAccessToken = response.headers['authorization']
+      if (newAccessToken?.startsWith('Bearer ')) {
+        newAccessToken = newAccessToken.substring(7)
+      }
 
-    setAccessToken(newAccessToken)
-    return newAccessToken
+      setAccessToken(newAccessToken)
+      return newAccessToken
+    })()
+
+    return await tokenRefreshPromise
   } catch (error) {
     console.error('Error during token reissue:', error)
     throw error
+  } finally {
+    // 토큰 재발급이 완료되면 상태 초기화
+    tokenRefreshInProgress = false
+    tokenRefreshPromise = null
   }
 }
