f2e957c579d769e7101e5d2e849afd6823bc61b4
yoonncho <nicealice99@gmail.com>
2025-05-11 22:27:04 +0900

    feat: 댓글 삭제 훅 추가 (#72)

---
+++ src/app/retrospects/_querys/useDeleteCommentMutation.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import openCustomToast from '@/utils/openCustomToast'
+
+import { deleteComment } from '../_lib'
+
+const useDeleteCommentMutation = (commentId: string, retrospectId: string) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async () => await deleteComment(commentId),
+    onSuccess: () => {
+      openCustomToast('댓글이 삭제되었습니다', true, '✅')
+
+      queryClient.invalidateQueries({
+        queryKey: ['getCommentList', { retrospectId }],
+      })
+    },
+  })
+}
+
+export default useDeleteCommentMutation
