d975b4364e4deff79e04c9d97fb1652ff4191662
leeleeleeleejun <wnstjr6293@naver.com>
2025-05-18 17:27:35 +0900

    feat: MemoListModal에 저장된 회고가 없을 때 표시하는 NoMemo 컴포넌트 추가 (#77)

--- src/app/retrospects/create/_components/MemoListModal.tsx
+++ src/app/retrospects/create/_components/MemoListModal.tsx
@@ -5,6 +5,7 @@ import Button from '@/components/Button'
 import { Icon } from '@/components/Icon'
 import { Fragment, useState } from 'react'
 import MemoListItem from './MemoListItem'
+// import { cn } from '@/utils/cn'
 
 const MemoListModal = ({
   isOpen,
@@ -32,6 +33,7 @@ const MemoListModal = ({
         </button>
       </div>
       <div className='text-caption01 text-gray-700 mb-2'>총 5개</div>
+      {/*<NoMemo />*/}
       <ul className='flex flex-col border border-gray-100 rounded-sm mb-8 overflow-hidden'>
         {/*Todo: memoId로 변경 필요*/}
         {[0, 1, 2, 3, 4].map((id, index) => (
@@ -76,3 +78,19 @@ export default MemoListModal
 const BottomBorder = () => {
   return <hr className='border-gray-100' />
 }
+
+// const NoMemo = () => {
+//   return (
+//     <div
+//       className={cn(
+//         'flex flex-col items-center justify-center',
+//         'text-body03',
+//         'bg-gray-50',
+//         'rounded-sm',
+//         'py-3 mb-8',
+//       )}
+//     >
+//       저장된 회고가 없습니다.
+//     </div>
+//   )
+// }
