86026951419e8b242a9512912c29898c9c5e8efe
leeleeleeleejun <wnstjr6293@naver.com>
2025-06-23 20:21:24 +0900

    feat: memo id가 null일 경우 조건부로 쿼리 실행 (#77)

--- src/app/retrospects/create/[[...memoId]]/_queries/useGetMemo.ts
+++ src/app/retrospects/create/[[...memoId]]/_queries/useGetMemo.ts
@@ -1,13 +1,15 @@
 import { getMemo } from '@/app/retrospects/create/[[...memoId]]/_lib'
 import { useQuery } from '@tanstack/react-query'
 
-const useGetMemo = (memoId: string) => {
+const useGetMemo = (memoId: string | null) => {
   return useQuery({
     queryKey: ['memo', memoId],
     queryFn: async () => {
+      if (!memoId) return null
       const { data } = await getMemo(memoId)
       return data
     },
+    enabled: !!memoId,
   })
 }
 
