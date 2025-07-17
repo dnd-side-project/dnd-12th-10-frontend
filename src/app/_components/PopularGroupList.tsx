68c5235e59879a085155d1ec6ea0d72ec0601e39
leeleeleeleejun <wnstjr6293@naver.com>
2025-07-17 14:10:31 +0900

    refactor: PopularGroupCard 컴포넌트에 rank prop 추가 및 UI 수정 (#89)

--- src/app/_components/PopularGroupList.tsx
+++ src/app/_components/PopularGroupList.tsx
@@ -23,6 +23,7 @@ const PopularGroupList = () => {
         <div className='flex flex-col gap-y-2'>
           {popularGroupList.slice(0, 3).map((props, index) => (
             <PopularGroupCard
+              rank={index + 1}
               isActive={currentActiveRetrospect === index}
               onMouseEnter={() => {
                 setCurrentActiveRetrospect(index)
@@ -43,43 +44,42 @@ const PopularGroupList = () => {
 export default PopularGroupList
 
 const PopularGroupCard = ({
+  rank,
   isActive,
   onMouseEnter,
   groupId,
   groupName,
   userCount,
   categoryNames,
-}: Group & { isActive: boolean; onMouseEnter: () => void }) => {
+}: Group & { rank: number; isActive: boolean; onMouseEnter: () => void }) => {
   return (
     <Link
       href={`${URL_PATH.GroupList}/${groupId}`}
       onMouseEnter={onMouseEnter}
-      className={cn(
-        'px-6 py-[18px]',
-        'bg-white',
-        'rounded-md',
-        'w-[328px]',
-        'shadow-gray',
-        'hover:bg-[#FEFCF9]',
-      )}
+      className={cn('px-6 py-[18px]', 'rounded-md', 'flex gap-6', 'w-[328px]', {
+        'bg-white shadow-gray': isActive,
+      })}
     >
-      <div className='flex gap-x-1 mb-1'>
-        {categoryNames.map((tag, index) => (
-          <Chip
-            key={`group-tag-${index}`}
-            label={tag}
-            color='gray'
-            size='small'
-          />
-        ))}
-      </div>
-      <p className='text-title02 font-semibold'>{groupName}</p>
-      {isActive && (
-        <div className='text-gray-400 text-body02 mt-2 flex items-center gap-x-1'>
-          <Icon name='profile-filled' className='fill-gray-400' size={18} />
-          멤버 {userCount}명
+      <div className='text-title02 text-blue-500'>{rank}</div>
+      <div className='flex flex-col gap-3 flex-1'>
+        <p className='text-title02 font-semibold'>{groupName}</p>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-x-1'>
+            {categoryNames.map((tag, index) => (
+              <Chip
+                key={`group-tag-${index}`}
+                label={tag}
+                color={isActive ? 'lightBlue' : 'linear'}
+                size='small'
+              />
+            ))}
+          </div>
+          <div className='text-gray-400 text-body02 flex items-center gap-x-1'>
+            <Icon name='profile-filled' className='fill-gray-400' size={18} />
+            멤버 {userCount}명
+          </div>
         </div>
-      )}
+      </div>
     </Link>
   )
 }
