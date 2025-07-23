247370c7f2e815bc72058de3981bd0d6172e817e
leeleeleeleejun <wnstjr6293@naver.com>
2025-07-23 21:28:41 +0900

    feat: 클립보드 복사 후 사용자에게 알림을 표시하는 기능삭제 후 copyToCurrentUrl에서 toast로 표시 - openCustomToast prop에 position 필드 추가 (#92)

--- src/app/groups/[id]/_utils/copyToCurrentUrl.ts
+++ src/app/groups/[id]/_utils/copyToCurrentUrl.ts
@@ -1,8 +1,10 @@
 import copyToClipboard from '@/app/groups/[id]/_utils/copyToClipboard'
+import openCustomToast from '@/utils/openCustomToast'
 
-const copyToCurrentUrl = () => {
+const copyToCurrentUrl = async () => {
   const currentUrl = window.document.location.href
-  copyToClipboard(currentUrl)
+  await copyToClipboard(currentUrl)
+  openCustomToast('주소가 복사되었습니다.', false, '', 'bottom-center')
 }
 
 export default copyToCurrentUrl
