18783f20550383e10166f2c977c5dbc8735d6004
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-02 11:57:12 +0900

    feat: 랜딩 페이지 방문 여부 확인을 컴포넌트 마운트 시 실행하도록 useEffect로 래핑 (#105)

--- src/app/landing/_components/LandingVisitGuard.tsx
+++ src/app/landing/_components/LandingVisitGuard.tsx
@@ -3,13 +3,16 @@
 import { useRouter } from 'next/navigation'
 import { getLocalStorage } from '@/utils/storage'
 import { HAS_VISITED_LANDING_KEY } from '../_consts'
+import { useEffect } from 'react'
 
 const LandingVisitGuard = () => {
   const router = useRouter()
-  const hasVisitedLanding = getLocalStorage(HAS_VISITED_LANDING_KEY, '')
-  if (!hasVisitedLanding) {
-    router.replace('/landing')
-  }
+  useEffect(() => {
+    const hasVisitedLanding = getLocalStorage(HAS_VISITED_LANDING_KEY, '')
+    if (!hasVisitedLanding) {
+      router.replace('/landing')
+    }
+  }, [])
 
   return null
 }
