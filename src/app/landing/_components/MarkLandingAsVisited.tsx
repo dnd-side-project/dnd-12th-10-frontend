c4eb10d7117b90b94d0e9899309565b6426cd3d0
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-02 11:51:11 +0900

    feat: 랜딩 페이지 방문 여부를 컴포넌트 마운트 후 실행되도록 useeffect로 래핑 (#105)

--- src/app/landing/_components/MarkLandingAsVisited.tsx
+++ src/app/landing/_components/MarkLandingAsVisited.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { useEffect } from 'react'
 import { setLocalStorage } from '@/utils/storage'
 import { HAS_VISITED_LANDING_KEY } from '@/app/landing/_consts'
 
 const MarkLandingAsVisited = () => {
-  setLocalStorage(HAS_VISITED_LANDING_KEY, true)
+  useEffect(() => {
+    setLocalStorage(HAS_VISITED_LANDING_KEY, true)
+  }, [])
   return null
 }
 
