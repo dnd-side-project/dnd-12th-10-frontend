1392c6a67e3e64e9b31fb9444141acbe76b24eda
yoonncho <nicealice99@gmail.com>
2025-08-11 16:10:30 +0900

    fix: 랜딩 메인 페이지 이미지 변경 (#107)

--- src/app/landing/_components/sections/Section1.tsx
+++ src/app/landing/_components/sections/Section1.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image'
 import { cn } from '@/utils/cn'
 import { Icon } from '@/components/Icon'
 import Button from '@/components/Button'
-import MainFullImg from '@/assets/images/landing/MainFullImg.png'
-import MainNoSideImg from '@/assets/images/landing/MainNoSideImg.png'
+import MainFullImage from '@/assets/images/landing/MainFullImage.png'
 
 const Section1 = () => {
   const { push } = useRouter()
@@ -15,53 +14,32 @@ const Section1 = () => {
   }
 
   return (
-    <section className='flex justify-around bg-blue-50 pt-[100px] pb-[140px] px-[160px]'>
-      <div className='flex flex-col justify-center'>
-        <p className='text-3xl font-semibold leading-[130%] text-gray-900 mb-4'>
-          개인과 모임이 성찰을 통해
-          <br />
-          함께 성장하는 회고 서비스
-        </p>
-        <Icon name='logo' width={158} height={48} className='fill-black' />
-        <div className='mt-12'>
-          <Button
-            color='primary'
-            variant='filled'
-            size='medium'
-            onClick={handleClickHome}
-          >
-            시작하기
-          </Button>
+    <section className='flex bg-blue-50 pt-[100px] pb-14 px-[160px]'>
+      <div className='flex gap-x-10'>
+        <div className='flex flex-col justify-center'>
+          <p className='text-3xl font-semibold leading-[130%] text-gray-900 mb-4'>
+            개인과 모임이 성찰을 통해
+            <br />
+            함께 성장하는 회고 서비스
+          </p>
+          <Icon name='logo' width={158} height={48} className='fill-black' />
+          <div className='mt-12'>
+            <Button
+              color='primary'
+              variant='filled'
+              size='medium'
+              onClick={handleClickHome}
+            >
+              시작하기
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className='relative flex bg-black p-2 rounded-md'>
-        <Image
-          width={627}
-          height={446}
-          src={MainFullImg}
-          alt='MainFullImg'
-          className='rounded-sm'
-          quality={100}
-          sizes='700px'
-        />
-        <div
-          className={cn(
-            'absolute top-[18px] left-[116px]',
-            'h-[578px]',
-            'overflow-hidden',
-          )}
-        >
-          <Image
-            width={519}
-            height={400}
-            src={MainNoSideImg}
-            alt='MainNoSideImg'
-            sizes='700px'
-          />
+        <div className='relative flex max-w-full'>
+          <Image src={MainFullImage} width={661} height={695} alt='' />
           <div
             className={cn(
               'absolute bottom-0',
-              'w-full h-32',
+              'w-full h-1/5',
               'bg-gradient-to-b from-[rgba(240,248,255,0)] via-blue-50 to-blue-50',
             )}
           />
